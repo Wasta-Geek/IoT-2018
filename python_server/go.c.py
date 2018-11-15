8f946b57ab13f1453bd372cb866d1597f2154f06
Antoine Lempereur <antoine.lempereur@epitech.eu>
2018-11-15 18:49:48 +0100

    setup http + pairing

--- python_server/go.c.py
+++ python_server/go.c.py
@@ -1,25 +1,58 @@
 import paho.mqtt.client as mqtt_client
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import time
+import json
 
 broker = "broker.mqttdashboard.com"
 root_topic = "doom_portal/"
+lastPairingAttempt = 0
+authenticatedDevices = []
 
 
 def unlock_door(payload):
     print("Trying to unlock door with payload " + str(payload))
+    # todo : découper le visage avec openCV. Si openCV trouve pas de visage on renvoit try again,
+    # sinon on envoit le visage à l'API de face recognition qui doit nous dire si c'est ok ou pas
+    # si c'est pas ok on envoit une notification push à l'app
+
+
+# HTTP
+def manually_unlock_door(payload):
+    print("Trying to manually unlock door with payload " + str(payload))
+
+
+# HTTP
+def feed_whitelist_info(payload):
+    print("Trying to manually unlock door with payload " + str(payload))
 
 
 def await_pairing(payload):
     print("Trying to pair device with payload " + str(payload))
+    global lastPairingAttempt
+    lastPairingAttempt = time.time()
 
 
-def confirm_pairing(payload):
-    print("Trying to confirm device pairing with payload " + str(payload))
+# HTTP
+def confirm_pairing(deviceId):
+    print("Trying to confirm device pairing")
+    if time.time() - lastPairingAttempt > 10:
+        print("Did not authorize " + deviceId + " to access API because the last pairing attempt is too old")
+        return 403
+    if deviceId in authenticatedDevices:
+        print("Did not authorize " + deviceId + " to access API because the device ID is already registered")
+        return 409
+    authenticatedDevices.append(deviceId)
+    print("Authorized " + deviceId + " to access API")
+    return 201
 
+
+# ==============MQTT SETUP===============
 
 mqtt_protocol = {
     root_topic + "unlock_door": unlock_door,
     root_topic + "await_pairing": await_pairing,
-    root_topic + "confirm_pairing": confirm_pairing
+    # root_topic + "confirm_pairing": confirm_pairing
 }
 
 
@@ -42,4 +75,23 @@ print("Connecting to broker ", broker)
 mqtt_client.connect(broker)
 
 
-mqtt_client.loop_forever()
+mqtt_client.loop_start()
+
+# ==============HTTP SETUP===============
+
+
+class Pairing(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("deviceId")
+        args = parser.parse_args()
+        return confirm_pairing(args["deviceId"])
+
+
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(Pairing, "/pair")
+
+app.run("127.0.0.1", 8080, use_reloader=False, debug=True)
+
+mqtt_client.loop_stop()
